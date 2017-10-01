6bd071be90910830940cd4ed1d3d9101474cd6b6
Doken61 <32429692+Doken61@users.noreply.github.com>
2017-10-01 12:01:32 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'pelin') {
+    msg.reply('Gakupo!');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'gakupo') {
+    msg.channel.sendMessage('Sevi-seviyorum Gakupo♥');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Gakupo') {
+    msg.channel.sendMessage('Ölene kadar seviyorum samurayım♥');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === 'Sa') {
+    msg.reply('Aley- Aleyk- Merhaba!');
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
