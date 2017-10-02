55be45fffc89b8de20eff57f53bf0a2cc6b2a910
Doken61 <32429692+Doken61@users.noreply.github.com>
2017-10-02 16:40:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,15 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+var prefix = '?' ;
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
+
+client.on('message', msg => {
+  if (!msg.content.startsWith(prefix)) {
+    return;
 
 client.on('message', msg => {
   if (msg.content.ToLowerCase() === 'pelin') {
